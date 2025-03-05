ffb7225b85156988c8d3ba4f0cd04e297480c365
Max Choheli <max.choheli@gmail.com>
2025-03-05 18:56:12 +0200

    added ballsize increase

--- js/main.js
+++ js/main.js
@@ -5,5 +5,15 @@ toggleButton.addEventListener("click", () => {
 })
 
 function ballClick() {
-    console.log("ball has been clicked")
+    var ball = document.querySelector(".ball")
+    var curWidth = parseInt(ball.style.width) || 100
+    var curHeight = parseInt(ball.style.height) || 100
+
+    var newWidth = curWidth + 50
+    var newHeight = curHeight + 50
+
+    ball.style.width = `${newWidth}px`
+    ball.style.height = `${newHeight}px`
+
+    ball.textContent = newWidth
 }
